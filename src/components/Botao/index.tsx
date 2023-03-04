4119d07fe54781126240b7557f1c942a26685220
Wesley Isaias <wesley_isaias@hotmail.com>
2023-03-04 10:39:09 -0300

    communication between components

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,16 @@
+ 
+ type Props = {
+    text: string;
+    clickFn: (txt:string) => void ;
+ }
+ 
+ export const Botao = ({text, clickFn}: Props) => {
+
+   const handleClick = () => {
+      clickFn ("Fras");
+   }
+
+    return (
+        <button onClick={handleClick}>{text}</button>
+    );
+ }
